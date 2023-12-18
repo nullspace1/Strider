63281c1ab46e97f8d1a0ee5ab57952666a286d99
nullspace1 <lautaromoyano420@gmail.com>
2023-12-18 11:01:42 -0300

    0.0.1 first commit

--- strider/backend/main.js
+++ strider/backend/main.js
@@ -28,6 +28,27 @@ knex.schema.hasTable('athletes').then(exists => {
     }
 })
 
+knex.schema.hasTable('calendars').then(exists => {
+    if (!exists) {
+        return knex.schema.createTable('calendars', table => {
+            table.increments('id').primary();
+            table.string('name');
+        })
+    }
+})
+
+knex.schema.hasTable('activities').then(exists => {
+    if (!exists) {
+        return knex.schema.createTable('activities', table => {
+            table.increments('id').primary();
+            table.string('name');
+            table.integer('week');
+            table.integer('day');
+            table.integer('calendar_id').references('id').inTable('calendars').onDelete('cascade');
+        })
+    }
+}) 
+
 
 
 function createWindow() {
@@ -56,26 +77,66 @@ function createWindow() {
     
 }
 
+ipcMain.handle('get:calendar_identifiers_list', async (event) => {
+    const calendar_values = await knex('calendars').select('id').select('name')
+    return calendar_values;
+})
+
+ipcMain.handle('save:calendar', async(event, calendarName) => {
+    await knex('calendars').insert({name: calendarName}).onConflict('id').merge();
+})
+
 ipcMain.handle('get:athlete_identifiers', async (event,params) => {
     const athlete_values = await knex('athletes').select('id').select('name')
     return athlete_values;
 })
 
 ipcMain.handle('save:athlete', async(event,athlete) => {
+    delete athlete.age
     await knex('athletes').insert(athlete).onConflict('id').merge()
 })
 
 ipcMain.handle('get:athlete_full_info', async (event, athlete)  => {
-    const athlete_full =await knex('athletes').select().where('id',athlete.id).first()
+    const athlete_full = await knex('athletes').select().where('id',athlete.id).first()
+    athlete_full.age = new Date().getFullYear() - new Date(athlete_full.birthday).getFullYear()
     return athlete_full
 })
 
+ipcMain.handle('delete:athlete', async (event,athlete) => {
+    await knex('athletes').delete().where('id',athlete.id)
+})
 
+ipcMain.handle('get:full_calendar_info', async (event, calendar) => {
+    const calendar_full = await knex('calendars').select().where('id', calendar.id).first();
+    const activities = await knex('activities').select().where('calendar_id', calendar.id);
+
+    let organizedActivities = new Array(52).fill(null).map(() => new Array(7).fill(null));
+
+    activities.forEach(activity => {
+        const week = activity.week; 
+        const day = activity.day;   
+
+        if (week >= 1 && week <= 52 && day >= 1 && day <= 7) {
+            if (!organizedActivities[week - 1][day - 1]) {
+                organizedActivities[week - 1][day - 1] = [];
+            }
+            organizedActivities[week - 1][day - 1].push(activity);
+        }
+    });
+
+    return {
+        calendar: calendar_full,
+        activities: organizedActivities
+    };
+});
+
+ipcMain.handle('delete:calendar', async (event, calendar) => {
+    await knex('calendars').delete().where('id',calendar.id)
+})
 
 
 
 
 app.removeAllListeners('ready');
-
 app.whenReady().then(createWindow);
 
--- strider/src/App.js
+++ strider/src/App.js
@@ -1,20 +1,62 @@
-import { ActivitySection } from "./components/activity.js";
-import { AthleteSection } from "./components/athlete.js";
-import { Header } from "./components/header.js";
-import {CalendarSection} from './components/plan.js'
-import './components/css/styles.css'
+import React from "react";
+import {useQuery} from "react-query";
+import './css/app.css'
+
+const { ipcRenderer } = window.require('electron')
+
 
 const App = () => {
 
+    const fetchAthletes = async () => {
+        const athletes = await ipcRenderer.invoke('GET athlete');
+        return athletes;
+    };
 
-  return (
-    <div className={'container'}>
-        <Header />
-        <CalendarSection/>
-        <ActivitySection/>
-        <AthleteSection/>
-    </div>
-  );
+    const fetchCalendars = async () => {
+        const calendars = await ipcRenderer.invoke('GET calendars');
+        return calendars;
+    };
+
+    const [calendars, setCalendars] = React.useState([]);
+    const [athletes, setAthletes] = React.useState([]);
+
+    const {fetchedCalendars, statusCalendars} = useQuery('calendar', fetchCalendars);
+    const {fetchedAthletes, statusAthletes} = useQuery('athlete', fetchAthletes);
+
+    React.useEffect(() => {
+        setCalendars(fetchedCalendars)
+    },[calendars]);
+
+    React.useEffect(() => {
+        setAthletes(fetchedAthletes);
+    },[athletes]);
+
+    return (
+        <div className='App'>
+            <Calendar calendars={calendars} />
+            <Athlete athletes={athletes}/>
+        </div>
+    );
+
 };
- 
+
+function Calendar() {
+
+    return (
+        <div className='Calendar'>
+            
+        </div>
+    )
+
+
+}
+
+function Athlete() {
+    return (
+        <div className='Athlete'>
+           
+        </div>
+    )
+}
+
 export default App;
